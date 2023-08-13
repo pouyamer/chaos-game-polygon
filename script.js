21711a9206169d0f874c68355049f4dc29dd3a6e
Pouya Moazzezi <pmoazzezi@gmail.com>
2023-08-13 17:10:51 +0330

    Added Hue Offset Fixed Typo removed a not used variable

--- script.js
+++ script.js
@@ -9,24 +9,24 @@ canvas.width = canvasSize.width
 const center = new Point(0, 0, "red")
 
 // drawing the polygon
-const { circleContainingPoligonPointsRadius } = config.poligon
-const poligonPoints = Array(config.poligon.sides.amount)
+const { circleContainingPoligonPointsRadius } = config.polygon
+const polygonPoints = Array(config.polygon.sides.amount)
   .fill()
   .map(
     (_, i, sideArray) =>
       new Point(
         circleContainingPoligonPointsRadius *
           Math.cos(
-            (i * 2 * Math.PI + config.poligon.rotationAngle_radian) /
+            (i * 2 * Math.PI + config.polygon.rotationAngle_radian) /
               sideArray.length
           ),
         circleContainingPoligonPointsRadius *
           Math.sin(
-            (i * 2 * Math.PI + config.poligon.rotationAngle_radian) /
+            (i * 2 * Math.PI + config.polygon.rotationAngle_radian) /
               sideArray.length
           ),
         new HslColor(
-          (360 * i) / config.poligon.sides.amount,
+          (360 * i) / config.polygon.sides.amount + config.polygon.hueOffset,
           70,
           50,
           1
@@ -35,32 +35,25 @@ const poligonPoints = Array(config.poligon.sides.amount)
   )
 
 // drawing the innerPoints
-const innerPointsColorBasedOnPolygonPointIndex = Array(
-  config.poligon.sides.amount
-)
-  .fill()
-  .map((_, i) =>
-    new HslColor((360 * i) / config.poligon.sides.amount, 70, 60, 1).toString()
-  )
 
 let currentPolygonPointIndex = Math.floor(
-  Math.random() * config.poligon.sides.amount
+  Math.random() * config.polygon.sides.amount
 )
 let currentPoint = center.getNewPointFromFractionOfDistanceBetweenTwoPoints(
-  poligonPoints[currentPolygonPointIndex],
+  polygonPoints[currentPolygonPointIndex],
   config.ratioFactor
 )
 
 const drawTheChaosGame = () => {
-  for (let i = 0; i < config.pointsDrawnPerFrame; i++) {
+  for (let i = 0; i < config.iterationsPerFrame; i++) {
     // get all unselected possible points on polygon
-    const poligonPointsExceptTheCurrentPoint = poligonPoints.filter(
+    const polygonPointsExceptTheCurrentPoint = polygonPoints.filter(
       (_, i) => i !== currentPolygonPointIndex
     )
 
     currentPolygonPointIndex =
-      poligonPointsExceptTheCurrentPoint[
-        Math.floor(Math.random() * poligonPointsExceptTheCurrentPoint.length)
+      polygonPointsExceptTheCurrentPoint[
+        Math.floor(Math.random() * polygonPointsExceptTheCurrentPoint.length)
       ]
 
     currentPoint =
@@ -78,21 +71,19 @@ const drawTheChaosGame = () => {
 ctx.fillStyle = config.canvas.backgroundColor
 ctx.fillRect(0, 0, canvas.height, canvas.width)
 
-//  drawing the poligon
+//  drawing the polygon
 //          shape:
-poligonPoints.forEach((point, i) => {
+polygonPoints.forEach((point, i) => {
   point.lineTo(
     ctx,
-    poligonPoints[i === poligonPoints.length - 1 ? 0 : i + 1],
-    config.poligon.sides.color,
-    config.poligon.sides.thickness
+    polygonPoints[i === polygonPoints.length - 1 ? 0 : i + 1],
+    config.polygon.sides.color,
+    config.polygon.sides.thickness
   )
 
   //         points:
-  config.poligon.showTrianglePoints &&
-    poligonPoints.forEach(point => point.draw(ctx))
+  config.polygon.showTrianglePoints &&
+    polygonPoints.forEach(point => point.draw(ctx))
 })
 currentPoint.draw(ctx)
 drawTheChaosGame()
-
-console.log(innerPointsColorBasedOnPolygonPointIndex)
