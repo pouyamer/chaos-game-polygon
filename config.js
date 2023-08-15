c2077a4c8a9c2914511784b63563d2ceed2ab02b
Pouya Moazzezi <pmoazzezi@gmail.com>
2023-08-16 01:44:30 +0330

    added new configurable values.

--- config.js
+++ config.js
@@ -1,27 +1,46 @@
 const config = {
   canvas: {
     size: { width: innerWidth, height: innerHeight },
-    backgroundColor: "hsl(20, 80%, 1%)"
+    backgroundColor: "#111"
   },
   polygon: {
     sides: {
       amount: 5,
-      color: "hsl(20,75%,60%,.3)",
-      thickness: 1
+      color: "hsl(20,75%,60%,.2)",
+      thickness: 3
     },
-    showTrianglePoints: false,
+    showPolygonPoints: false,
     rotationAngle_radian: Math.PI / 2,
     // a perfect polygon is drawn within a circle,
     // The wider the circle, bigger the sides
     circleContainingPoligonPointsRadius:
-      (0.8 * Math.min(innerHeight, innerWidth)) / 2,
+      (0.8 * Math.min(innerHeight, innerWidth)) / 2
     // Hue Offset: hue that gets added to hue of polygon points and associated points
-    hueOffset: 20
   },
   points: {
-    radius: 1
+    radius: 3,
+
+    color: {
+      hueOffset: 0,
+      // saturation and lightness of all points:
+      //  [0 - 100]
+      saturation: 85,
+      lightness: 65,
+      // alpha value: [0 - 1]
+      alpha: 1
+    },
+    // coloringMode values:
+    //   "takeAverage" | "secondPoint" |
+    //   "firstPoint"  | "addHues"  |
+    //   "randomBetweenTheTwo"
+    coloringMode: "takeAverage"
   },
-  // iterationsPerFrame: the higher it goes, more resourses it'll consume
-  iterationsPerFrame: 100,
-  ratioFactor: 1 / 1.615
+  // WARNING : the higher `iterationsPerFrame` goes, more resourses it'll consume
+  iterationsPerFrame: 10,
+
+  // indicates That each update should occur on what frame
+  //  (default : 1)
+  // animation fps = your_fps / updateOnFrameIndex
+  updateOnFrameIndex: 2,
+  ratioFactor: 2 / 3
 }
