b67f2f43771f17d6834fa17ecae33beb806bd63c
Pouya Moazzezi <pmoazzezi@gmail.com>
2023-08-27 16:21:09 +0330

    added toRgb function

--- src/classes/HslColor.ts
+++ src/classes/HslColor.ts
@@ -11,6 +11,42 @@ class HslColor implements IHslColor {
     this.alpha = alpha
   }
 
+  toRgb = () => {
+    const hueRatio = this.hue / 360
+    const saturationRatio = this.saturation / 100
+    const lightnessRatio = this.lightness / 100
+
+    let red, green, blue
+
+    if (saturationRatio === 0) {
+      red = green = blue = lightnessRatio
+    } else {
+      const hueToRgb = (p: number, q: number, t: number) => {
+        if (t < 0) t += 1
+        if (t > 1) t -= 1
+        if (t < 1 / 6) return p + (q - p) * 6 * t
+        if (t < 1 / 2) return q
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+        return p
+      }
+
+      const q =
+        lightnessRatio < 0.5
+          ? lightnessRatio * (1 + saturationRatio)
+          : lightnessRatio + saturationRatio - lightnessRatio * saturationRatio
+      const p = 2 * lightnessRatio - q
+      red = hueToRgb(p, q, hueRatio + 1 / 3)
+      green = hueToRgb(p, q, hueRatio)
+      blue = hueToRgb(p, q, hueRatio - 1 / 3)
+    }
+
+    red = Math.round(red * 255)
+    green = Math.round(green * 255)
+    blue = Math.round(blue * 255)
+
+    return new RgbColor(red, green, blue, this.alpha)
+  }
+
   toString = () =>
     `hsl(${this.hue},${this.saturation}%,${this.lightness}%,${this.alpha})`
 }
